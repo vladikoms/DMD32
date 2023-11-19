18db15c106dd40ce289f35235786690840867b81
Vladimir <vladikoms@gmail.com>
2023-11-19 15:20:18 +1000

    Update dmd_demo.ino

--- examples/dmd_demo/dmd_demo.ino
+++ examples/dmd_demo/dmd_demo.ino
@@ -1,173 +1,171 @@
-/*--------------------------------------------------------------------------------------
-
- dmd_test.cpp 
-   Demo and example project for the Freetronics DMD, a 512 LED matrix display
-   panel arranged in a 32 x 16 layout.
-
-This LIibrary (DMD32) and example are  fork of original DMD library  was modified to work on ESP32 
-Modified by: Khudhur Alfarhan  // Qudoren@gmail.com
-1/Oct./2020
-
- See http://www.freetronics.com/dmd for resources and a getting started guide.
-
- Note that the DMD32 library uses the VSPI port for the fastest, low overhead writing to the
- display. Keep an eye on conflicts if there are any other devices running from the same
- SPI port, and that the chip select on those devices is correctly set to be inactive
- when the DMD is being written to.
-
- USAGE NOTES
- -----------
- - Place the DMD library folder into the "arduino/libraries/" folder of your Arduino installation.
- - Restart the IDE.
- - In the Arduino IDE, go to Tools > Board > and choose any ESP32 board
- - In the Arduino IDE, you can open File > Examples > DMD > dmd_demo, or dmd_clock_readout, and get it
-   running straight away!
-
- * See the documentation on Github or attached images to find the pins that should be connected to the DMD LED display
-  
-
- This example code is in the public domain.
- The DMD32 library is open source (GPL), for more see DMD32.cpp and DMD32.h
-
---------------------------------------------------------------------------------------*/
-
-/*--------------------------------------------------------------------------------------
-  Includes
---------------------------------------------------------------------------------------*/
-#include <DMD32.h>        //
+//>>>>>>>>>>>>>>>> 01_ESP32_P10_Test_Displays
+//----------------------------------------Including the libraries.
+#include <DMD32.h>  //--> DMD32 by Qudor-Engineer (KHUDHUR ALFARHAN) : https://github.com/Qudor-Engineer/DMD32
 #include "fonts/SystemFont5x7.h"
 #include "fonts/Arial_black_16.h"
+//----------------------------------------
 
-//Fire up the DMD library as dmd
+// Fire up the DMD library as dmd.
 #define DISPLAYS_ACROSS 1
 #define DISPLAYS_DOWN 1
 DMD dmd(DISPLAYS_ACROSS, DISPLAYS_DOWN);
 
-   //Timer setup
-  //create a hardware timer  of ESP32
-  hw_timer_t * timer = NULL;
+// Timer setup.
+// create a hardware timer  of ESP32.
+hw_timer_t * timer = NULL;
 
-/*--------------------------------------------------------------------------------------
-  Interrupt handler for Timer1 (TimerOne) driven DMD refresh scanning, this gets
-  called at the period set in Timer1.initialize();
---------------------------------------------------------------------------------------*/
-void IRAM_ATTR triggerScan()
-{
+//________________________________________________________________________________IRAM_ATTR triggerScan()
+//  Interrupt handler for Timer1 (TimerOne) driven DMD refresh scanning,
+//  this gets called at the period set in Timer1.initialize();
+void IRAM_ATTR triggerScan() {
   dmd.scanDisplayBySPI();
 }
+//________________________________________________________________________________
 
-/*--------------------------------------------------------------------------------------
-  setup
-  Called by the Arduino architecture before the main loop begins
---------------------------------------------------------------------------------------*/
-void setup(void)
-{
+//________________________________________________________________________________VOID SETUP()
+void setup() {
+  // put your setup code here, to run once:
 
-// return the clock speed of the CPU
+  Serial.begin(115200);
+  Serial.println();
+
+  delay(500);
+
+  Serial.println();
+  Serial.println("return the clock speed of the CPU.");
+  // return the clock speed of the CPU.
   uint8_t cpuClock = ESP.getCpuFreqMHz();
-  
-  // Use 1st timer of 4 
-  // devide cpu clock speed on its speed value by MHz to get 1us for each signal  of the timer
+  delay(500);
+
+  Serial.println();
+  Serial.println("Timer Begin");
+  // Use 1st timer of 4.
+  // devide cpu clock speed on its speed value by MHz to get 1us for each signal  of the timer.
   timer = timerBegin(0, cpuClock, true);
-  // Attach triggerScan function to our timer 
+  delay(500);
+
+  Serial.println();
+  Serial.println("Attach triggerScan function to our timer.");
+  // Attach triggerScan function to our timer.
   timerAttachInterrupt(timer, &triggerScan, true);
-  // Set alarm to call triggerScan function  
-  // Repeat the alarm (third parameter) 
+  delay(500);
+
+  Serial.println();
+  Serial.println("Set alarm to call triggerScan function.");
+  // Set alarm to call triggerScan function.
+  // Repeat the alarm (third parameter).
   timerAlarmWrite(timer, 300, true);
+  delay(500);
 
-  // Start an alarm 
+  Serial.println();
+  Serial.println("Start an alarm.");
+  // Start an alarm.
   timerAlarmEnable(timer);
-
-   //clear/init the DMD pixels held in RAM
-   dmd.clearScreen( true );   //true is normal (all pixels off), false is negative (all pixels on)
-
+  delay(500);
 }
+//________________________________________________________________________________
 
-/*--------------------------------------------------------------------------------------
-  loop
-  Arduino architecture main loop
---------------------------------------------------------------------------------------*/
-void loop(void)
-{
-   byte b;
-   
-   // 10 x 14 font clock, including demo of OR and NOR modes for pixels so that the flashing colon can be overlayed
-   dmd.clearScreen( true );
-   dmd.selectFont(Arial_Black_16);
-   dmd.drawChar(  0,  3, '2', GRAPHICS_NORMAL );
-   dmd.drawChar(  7,  3, '3', GRAPHICS_NORMAL );
-   dmd.drawChar( 17,  3, '4', GRAPHICS_NORMAL );
-   dmd.drawChar( 25,  3, '5', GRAPHICS_NORMAL );
-   dmd.drawChar( 15,  3, ':', GRAPHICS_OR     );   // clock colon overlay on
-   delay( 1000 );
-   dmd.drawChar( 15,  3, ':', GRAPHICS_NOR    );   // clock colon overlay off
-   delay( 1000 );
-   dmd.drawChar( 15,  3, ':', GRAPHICS_OR     );   // clock colon overlay on
-   delay( 1000 );
-   dmd.drawChar( 15,  3, ':', GRAPHICS_NOR    );   // clock colon overlay off
-   delay( 1000 );
-   dmd.drawChar( 15,  3, ':', GRAPHICS_OR     );   // clock colon overlay on
-   delay( 1000 );
+//________________________________________________________________________________VOID LOOP()
+void loop() {
+  // put your main code here, to run repeatedly:
 
-   dmd.drawMarquee("Scrolling Text",14,(32*DISPLAYS_ACROSS)-1,0);
-   long start=millis();
-   long timer=start;
-   boolean ret=false;
-   while(!ret){
-     if ((timer+30) < millis()) {
-       ret=dmd.stepMarquee(-1,0);
-       timer=millis();
-     }
-   }
-   // half the pixels on
-   dmd.drawTestPattern( PATTERN_ALT_0 );
-   delay( 1000 );
+  // If you want to change the font, don't forget to use this line of code : dmd.selectFont . Example: dmd.selectFont(Arial_Black_16);
 
-   // the other half on
-   dmd.drawTestPattern( PATTERN_ALT_1 );
-   delay( 1000 );
-   
-   // display some text
-   dmd.clearScreen( true );
-   dmd.selectFont(System5x7);
-   for (byte x=0;x<DISPLAYS_ACROSS;x++) {
-     for (byte y=0;y<DISPLAYS_DOWN;y++) {
-       dmd.drawString(  2+(32*x),  1+(16*y), "freet", 5, GRAPHICS_NORMAL );
-       dmd.drawString(  2+(32*x),  9+(16*y), "ronic", 5, GRAPHICS_NORMAL );
-     }
-   }
-   delay( 2000 );
-   
-   // draw a border rectangle around the outside of the display
-   dmd.clearScreen( true );
-   dmd.drawBox(  0,  0, (32*DISPLAYS_ACROSS)-1, (16*DISPLAYS_DOWN)-1, GRAPHICS_NORMAL );
-   delay( 1000 );
-   
-   for (byte y=0;y<DISPLAYS_DOWN;y++) {
-     for (byte x=0;x<DISPLAYS_ACROSS;x++) {
-       // draw an X
-       int ix=32*x;
-       int iy=16*y;
-       dmd.drawLine(  0+ix,  0+iy, 11+ix, 15+iy, GRAPHICS_NORMAL );
-       dmd.drawLine(  0+ix, 15+iy, 11+ix,  0+iy, GRAPHICS_NORMAL );
-       delay( 1000 );
-   
-       // draw a circle
-       dmd.drawCircle( 16+ix,  8+iy,  5, GRAPHICS_NORMAL );
-       delay( 1000 );
-   
-       // draw a filled box
-       dmd.drawFilledBox( 24+ix, 3+iy, 29+ix, 13+iy, GRAPHICS_NORMAL );
-       delay( 1000 );
-     }
-   }
+  //----------------------------------------Demo with "Arial_Black_16" font.
+  dmd.selectFont(Arial_Black_16);
 
-   // stripe chaser
-   for( b = 0 ; b < 20 ; b++ )
-   {
-      dmd.drawTestPattern( (b&1)+PATTERN_STRIPE_0 );
-      delay( 200 );      
+  //.................Running Text.
+  String txt_1 = "ESP32 with P10 LED Display";
+  char char_array_txt_1[txt_1.length() + 1];
+  txt_1.toCharArray(char_array_txt_1, txt_1.length() + 1);
+
+  dmd.clearScreen(true);
+  delay(1000);
+  dmd.drawMarquee(char_array_txt_1,txt_1.length(),(32*DISPLAYS_ACROSS)-1,0);
+  long start_1=millis();
+  long timer_1=start_1;
+  boolean ret=false;
+  while(!ret){
+   if ((timer_1+30) < millis()) {
+     ret=dmd.stepMarquee(-1,0);
+     timer_1=millis();
    }
-   delay( 200 );      
-   
+  }
+  //.................
+
+  //.................Display Text.
+  dmd.clearScreen(true);
+  delay(1000);
+  dmd.drawString(0,0,"DMD", 3, GRAPHICS_NORMAL);  //--> dmd.drawString(x, y, Text, Number of characters in text, GRAPHICS_NORMAL);
+  delay(3000);
+  //.................
+  //----------------------------------------
+
+  //----------------------------------------Demo with "SystemFont5x7" font.
+  // If you use the font "SystemFont5x7", then 1 panel P10 (32x16) can display text in 2 rows.
+  dmd.selectFont(SystemFont5x7);
+
+  //.................Display Text.
+  dmd.clearScreen(true);
+  delay(1000);
+  dmd.drawString(0,0,"ESP32", 5, GRAPHICS_NORMAL);
+  dmd.drawString(0,9,"P10", 3, GRAPHICS_NORMAL);
+  delay(3000);
+  //.................
+
+  //.................The first row displays text and the second row displays running text.
+  String txt_2 = "ESP32 with P10 LED Display";
+  char char_array_txt_2[txt_2.length() + 1];
+  txt_2.toCharArray(char_array_txt_2, txt_2.length() + 1);
+  int scrl_long = (txt_2.length()*6) + (32*DISPLAYS_ACROSS);
+
+  dmd.clearScreen(true);
+  delay(1000);
+  
+  // Displays text in the first row.
+  dmd.drawString(4,0,"UTEH", 4, GRAPHICS_NORMAL);
+  
+  long start_2=millis();
+  long timer_2=start_2;
+  int i = 32*DISPLAYS_ACROSS;
+  while(true){
+    if ((timer_2+30) < millis()) {
+      // Displays running text on the second row.
+      dmd.drawString(i, 9, char_array_txt_2, txt_2.length(), GRAPHICS_NORMAL);
+      
+      if (i > ~scrl_long) {
+        i--;
+      } else {
+        break;
+      }
+    
+      timer_2=millis();
+    }
+  }
+  //.................
+
+  //.................Displays Text and Numbers.
+  int T = 29;
+  int H = 73;
+  char char_array_T[String(T).length() + 1];
+  char char_array_H[String(H).length() + 1];
+  String(T).toCharArray(char_array_T, String(T).length() + 1);
+  String(H).toCharArray(char_array_H, String(H).length() + 1);
+
+  dmd.clearScreen(true);
+  delay(1000);
+  
+  dmd.drawString(0, 0, "T:", 2, GRAPHICS_NORMAL);
+  dmd.drawString(11, 0, char_array_T, String(T).length(), GRAPHICS_NORMAL);
+  dmd.drawCircle(24, 1, 1, GRAPHICS_NORMAL);
+  dmd.drawString(27, 0, "C", 1, GRAPHICS_NORMAL);
+  
+  dmd.drawString(0, 9, "H:", 2, GRAPHICS_NORMAL);
+  dmd.drawString(11, 9, char_array_H, String(H).length(), GRAPHICS_NORMAL);
+  dmd.drawString(27, 9, "%", 1, GRAPHICS_NORMAL);
+
+  delay(3000);
+  //.................
+  //----------------------------------------
 }
+//________________________________________________________________________________
